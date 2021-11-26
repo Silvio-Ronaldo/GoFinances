fe97cbace550c7f1239ae071dccc47f99ed7e974
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-11-26 03:59:58 -0300

    Add transactions list

---
+++ src/components/TransactionCard/styles.ts
@@ -0,0 +1,63 @@
+import styled from "styled-components/native";
+import { Feather } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+interface TypeProps {
+    type: 'positive' | 'negative';
+}
+
+export const Container = styled.View`
+    background-color: ${({ theme }) => theme.colors.shape};
+
+    padding: 17px 24px;
+    border-radius: 5px;
+    margin-bottom: 16px;
+`;
+
+export const Title = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(14)}px;
+`;
+
+export const Amount = styled.Text<TypeProps>`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(20)}px;
+
+    color: ${({ theme, type }) => 
+        type === 'positive' ? theme.colors.success : theme.colors.attention};
+    
+    margin-top: 2px;
+`;
+ 
+export const Footer = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    margin-top: 19px;
+`;
+
+export const Category = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Icon = styled(Feather)`
+    font-size: ${RFValue(20)}px;
+
+    color: ${({ theme }) => theme.colors.textAlternative};
+`;
+
+export const CategoryName = styled.Text`
+    font-size: ${RFValue(14)}px;
+
+    color: ${({ theme }) => theme.colors.textAlternative};
+
+    margin-left: 17px;
+`;
+
+export const Date = styled.Text`
+    font-size: ${RFValue(14)}px;
+    
+    color: ${({ theme }) => theme.colors.textAlternative};
+`;
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardData } from '../../components/TransactionCard';
 
 import { 
     Container, 
@@ -12,10 +13,53 @@ import {
     UserGreeting, 
     UserName,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionsList
 } from './styles';
 
+export interface DataListProps extends TransactionCardData {
+    id: string;
+}
+
 export function Dashboard() {
+    const data: DataListProps[] = [
+    {
+        id: '1',
+        type: 'positive',
+        title: 'Desenvolvimento de Site',
+        amount: 'R$ 7.500,00',
+        category: {
+            name: 'Vendas',
+            icon: 'dollar-sign',
+        },
+        date: '25/11/2021',
+    },
+    {
+        id: '2',
+        type: 'negative',
+        title: 'Livros na Amazon',
+        amount: 'R$ 2.315,00',
+        category: {
+            name: 'Compras',
+            icon: 'shopping-bag',
+        },
+        date: '25/11/2021',
+    },
+    {
+        id: '3',
+        type: 'negative',
+        title: 'Pastel de carne com caldo de cana',
+        amount: 'R$ 70,00',
+        category: {
+            name: 'Alimentação',
+            icon: 'coffee',
+        },
+        date: '25/11/2021',
+    }
+    ];
+
     return (
         <Container>
             <Header>
@@ -53,6 +97,17 @@ export function Dashboard() {
                     lastTransaction="01 à 25 de novembro"
                 />
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionsList 
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => 
+                        <TransactionCard data={item} />}
+                />
+            </Transactions>
         </Container>
     );
 }
